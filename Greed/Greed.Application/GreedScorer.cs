f74904985f8c6536748ec4408cceb9d178515d85
Ruudzaki <56171632+Ruudzaki@users.noreply.github.com>
2022-09-25 16:09:59 +0300

    implement a single one rule

---
+++ Greed/Greed.Application/GreedScorer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Greed.Application
+{
+    public class GreedScorer
+    {
+        private int Score { get; set; }
+        private const int SINGLE_ONE_MULTIPLIER = 100;
+
+        public int CalculateScore(IList<int> dices)
+        {
+            CheckSingleOne(dices);
+
+            return Score;
+        }
+
+        private IList<int> CheckSingleOne(IList<int> dices)
+        {
+            IList<int> dicesWithOnes = dices.Where(d => d == 1).ToList();
+
+            Score += dicesWithOnes.Count() * SINGLE_ONE_MULTIPLIER;
+
+            return dices.Except(dicesWithOnes).ToList();
+        }
+    }
+}
---
+++ Greed/Greed.Test/GreedScorerTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Greed.Application;
+using NUnit.Framework;
+
+namespace Greed.Test
+{
+    public class GreedScorerTest
+    {
+        [TestCase(new int[] {1, 2, 3, 4, 5}, ExpectedResult = 100)]
+        [TestCase(new int[] { 1, 1, 3, 4, 5}, ExpectedResult = 200)]
+        [TestCase(new int[] { 2, 2, 3, 4, 5}, ExpectedResult = 0)]
+
+        public int CalculateScore(int[] dices)
+        {
+            var scorer = new GreedScorer();
+
+            var score = scorer.CalculateScore(dices);
+
+            return score;
+        }
+    }
+}
